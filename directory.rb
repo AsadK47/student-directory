13da71ec1737a78660f644f7ee227026f61e2bab
Asad Khan <asad_khan47@hotmail.co.uk>
2018-10-22 14:49:26 +0100

    Student directory with interactive menu - now with both save and load functions

--- directory.rb
+++ directory.rb
@@ -35,6 +35,7 @@ def input_students
     hobby = "Basketball" if hobby.empty?
     country = gets.chomp.to_s
     country = "UK" if country.empty?
+
   end
   @students
 end
@@ -77,6 +78,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -99,6 +101,15 @@ def save_students
   file.close
 end
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort, hobby, country = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym, hobby: hobby.to_sym, country: country.to_sym}
+  end
+  file.close
+end
+
 def process(selection)
   case selection
   when "1"
@@ -107,6 +118,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit # This will cause the programme to terminate
   else
